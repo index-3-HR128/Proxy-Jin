12a20c02c5198424a11b80065fbe1bfddcee80ab
Sejin Park <sejinpark0209@gmail.com>
2020-06-30 14:31:24 -0700

    added git ignore

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
-
+//
 // const calendarProxy = proxy('/calendar', {target: 'localhost:3001/calendar'});
 // const reviewsProxy = proxy('/reviews', {target: 'localhost:3002/reviews'});
 // const carouselProxy = proxy('/carousel', {target: 'localhost:3003/carousel'});
